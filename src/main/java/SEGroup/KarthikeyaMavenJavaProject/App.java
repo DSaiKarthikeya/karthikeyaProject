85e238c48fe9b28bac2515bc09028f9ba313b554
karthik <karthikeyadaripalli@gmail.com>
2023-12-18 11:50:57 +0530

    1st

---
+++ src/main/java/SEGroup/KarthikeyaMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.KarthikeyaMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
